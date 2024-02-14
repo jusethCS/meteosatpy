8d3a2fa71ee6233bf8964e9a9c5a637bc0e69899
Juseth Chancay <juseth.chancay@gmail.com>
2024-02-13 20:09:51 -0500

    Added a function to parse NetCDF to GeoTIFF

--- meteosatpy/utils.py
+++ meteosatpy/utils.py
@@ -1,9 +1,12 @@
 import os
 import gzip
+import xarray
 import rasterio
+import subprocess
 import numpy as np
 import geopandas as gpd
 from rasterio.mask import mask
+from rasterio.transform import from_origin
 from fiona.crs import from_epsg
 from shapely.geometry import box
 
@@ -92,3 +95,79 @@ def writeRaster(raster, meta, path:str) -> None:
     """
     with rasterio.open(path, "w", **meta) as r:
         r.write(raster)
+
+
+def netcdf2TIFF(path:str, var:str, time:str, isflip:bool, out_path:str = None, 
+                correction:bool = False) -> None:
+    """
+    Parse a netcdf file to GeoTIFF
+    
+    Args:
+        path: File path of the netcdf file
+        var: Selected variable to write in the GeoTIFF file
+        time: Selected timestamp (yyyy-mm-dd HH:MM) to write in the GeoTIFF
+        out_path: Path where GeoTIFF will be write.
+        correction: Conditional to data correction for CMORPH case
+    """
+    # Remove the extension
+    if out_path==None:
+        out_path = path.replace(".nc", ".tif")
+
+    # Read the netcdf file
+    ds = xarray.open_dataset(path)
+    ds = ds.sel(time=time)
+
+    # Extract data and coordinates
+    data = ds[var].values
+    lat = ds['lat'].values
+    lon = ds['lon'].values
+
+    # Compute the spatial resolution
+    res_lat = abs(lat[1] - lat[0])
+    res_lon = abs(lon[1] - lon[0])
+
+    # Compute the spatial tranformation
+    lon_min = lon.min() - res_lon/2
+    lat_max = lat.max() + res_lat/2
+
+    # Correction
+    if correction==True:
+        mcd = data.shape[1] // 2
+        data = np.hstack((data[:, mcd:], data[:, :mcd]))
+        lon_min = lon_min - 180
+    
+    # Transform the projection considering correction
+    transform = from_origin(lon_min, lat_max, res_lon, res_lat)
+
+    # Raster metadata
+    meta = {"driver": "GTiff", 
+            "height": data.shape[0],
+            "width": data.shape[1],
+            "transform": transform,
+            "crs": '+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0',
+            "count" : 1, 
+            "dtype" :str(data.dtype)}
+    
+    # Flipping the image upright in the axis = 0 i.e., vertically
+    if isflip:
+        data = np.flip(data,0)
+
+    # Save data as GeoTIFF file
+    with rasterio.open(out_path, 'w', **meta) as dst:
+        dst.write(data, 1)
+
+
+def is_installed(program):
+    """
+    Determine if a program is installed
+    
+    Args:
+        program: name of the program
+    """
+    try:
+        subprocess.check_output([program, '--version'])
+        return True
+    except FileNotFoundError:
+        return False
+    except subprocess.CalledProcessError:
+        return True 
